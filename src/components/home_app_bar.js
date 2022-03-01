516977038365eb524ee0b286dd5b04e36255f106
Kaival Patel <39383435+Kaival-Patel@users.noreply.github.com>
2022-03-01 23:34:49 +0530

    Remove note logic added

--- src/components/home_app_bar.js
+++ src/components/home_app_bar.js
@@ -1,16 +1,27 @@
 import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { useDispatch } from 'react-redux';
 import AppBarContext from '../context/appbar_context';
+import { REMOVE_NOTE } from '../reducers/noteReducers';
 
 function HomeAppBar() {
   const data = useContext(AppBarContext);
+  const dispatch = useDispatch();
   return (
     <View style={style.appbarStyle}>
       <Text style={{fontSize: 17}}>Home</Text>
       {data.data.showDeleteButton ? (
-        <FontAwesome name="trash" size={25} color="#E42C5A" />
+        <TouchableOpacity
+          onPress={() => {
+            console.log('Deleting '+data.data.selectedIds);
+          
+            dispatch(REMOVE_NOTE(data.data.selectedIds));
+          }}>
+          <FontAwesome name="trash" size={25} color="#E42C5A" />
+        </TouchableOpacity>
       ) : (
         <View></View>
       )}
